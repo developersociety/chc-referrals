99cb892523550b39a1033159c84d99625d1e4e99
Suraj Vadgama <contact@suninthesky.co.uk>
2018-10-05 15:05:55 +0100

    Month picker tests passing

--- test/system/month_picker_test.rb
+++ test/system/month_picker_test.rb
@@ -1,9 +1,39 @@
 require 'application_system_test_case'
 
 class MonthPickerTest < ApplicationSystemTestCase
-  test 'month picker cant go into the future'
-  test 'month_picker has referrals'
-  test 'month_picker no referrals'
-  test 'month_picker updates usage'
-  test 'month_picker has no usage'
+  setup do
+    @user = create(:user)
+    @partner = create(:partner)
+    @referral = create(:referral, partner: @partner)
+  end
+
+  test 'month picker cannot go into the future' do
+    visit root_path
+    sign_in
+    assert_selector('.grey.no-decoration', text: '❮')
+  end
+
+  test 'month_picker has usage' do
+    visit root_path
+    sign_in
+    within("##{@partner.slug}-available-referrals") { assert_text('9') }
+  end
+
+  test 'month_picker no usage' do
+    visit root_path(date: 1.month.ago)
+    sign_in
+    within("##{@partner.slug}-available-referrals") { assert_text('10') }
+  end
+
+  test 'month_picker has referrals' do
+    visit referrals_path
+    sign_in
+    assert_text(@partner.name)
+  end
+
+  test 'month_picker no referrals' do
+    visit referrals_path(date: 1.month.ago)
+    sign_in
+    assert_text('No referrals this month.')
+  end
 end
